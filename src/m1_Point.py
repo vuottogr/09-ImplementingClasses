aafdcbf42f4f2dc554ed3e6d823b205724e8cc2b
vuottogr <vuottogr@rose-hulman.edu>
2019-02-08 20:46:00 -0500

    .

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -3,8 +3,8 @@ A simple   Point   class.
 NOTE: This is NOT rosegraphics -- it is your OWN Point class.
 
 Authors: David Mutchler, Vibha Alangar, Dave Fisher, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Elle Vuotto.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
